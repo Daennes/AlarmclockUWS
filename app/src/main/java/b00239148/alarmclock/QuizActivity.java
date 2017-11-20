b1e21f049d705857e63ad12ada5b05a6f728438e
Tomasz Rapusta <tomrap17@gmail.com>
2017-11-20 00:05:36 +0000

    Spotify

--- app/src/main/java/b00239148/alarmclock/QuizActivity.java
+++ app/src/main/java/b00239148/alarmclock/QuizActivity.java
@@ -1,12 +1,7 @@
 package b00239148.alarmclock;
 
 import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.ColorFilter;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,16 +9,45 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioGroup;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class QuizActivity extends AppCompatActivity {
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Pager;
+import kaaes.spotify.webapi.android.models.PlaylistSimple;
+import kaaes.spotify.webapi.android.models.PlaylistTrack;
+import kaaes.spotify.webapi.android.models.Track;
+
+public class QuizActivity extends AppCompatActivity  implements
+        SpotifyPlayer.NotificationCallback, ConnectionStateCallback  {
+
+    private static final String CLIENT_ID = "1c15dd3e98014e828bae9ddca23a2ac5";
+    private static final String REDIRECT_URI = "my-first-android-app://callback";
+
+    private static final int REQUEST_CODE = 1337;
+    private static int currentSongIndex = -1;
+    private static List<PlaylistTrack> allTracks;
+
+    private Player mPlayer;
+    private SpotifyService spotify;
 
     SongQuiz Quiz;
     int rightAnswer = 0;
@@ -36,6 +60,14 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
 
         //Test data exchange between Activities
         Intent test_intent = getIntent();
@@ -88,6 +120,140 @@ public class QuizActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d("QuizActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+
+            default:
+                break;
+        }
+    }
+
+    public List<PlaylistTrack> getAllTracks() {
+        return allTracks;
+    }
+
+    public Track getCurrentSong() {
+        return allTracks.get(currentSongIndex).track;
+    }
+
+    public void getPlaylist(final String playlistName) {
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Pager<PlaylistSimple> myPlaylists = spotify.getMyPlaylists();
+                    for (PlaylistSimple playlistSimpleItem : myPlaylists.items) {
+                        if (playlistSimpleItem.name.equals(playlistName)) {
+                            Pager<PlaylistTrack> playlistTracks = spotify.getPlaylistTracks(playlistSimpleItem.owner.id, playlistSimpleItem.id);
+                            allTracks = playlistTracks.items;
+                            break;
+                        }
+                    }
+                    if (allTracks == null) {
+                        throw new Exception("No playlist found with a name: " + playlistName);
+                    }
+                    if (allTracks.size() == 0) {
+                        throw new Exception("Playlist with a name " + playlistName + " is empty.");
+                    }
+                    Collections.shuffle(allTracks);
+                } catch (Exception e) {
+                    Log.e("Error", e.getMessage());
+                }
+            }
+        });
+        t.start();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            Log.e("Error", e.getMessage());
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("QuizActivity", "Playback error received: " + error.name());
+        switch (error) {
+            default:
+                break;
+        }
+    }
+
+    public void playNextSong() {
+
+        if(currentSongIndex<allTracks.size()-1) {
+            currentSongIndex = currentSongIndex+1;
+            mPlayer.playUri(null, allTracks.get(currentSongIndex).track.uri, 0, 0);
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("QuizActivity", "User logged in");
+
+        //todo provide playlist name from options
+        getPlaylist("My Shazam Tracks");
+        playNextSong();
+
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("QuizActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Error error) {
+        Log.d("QuizActivity", "Login failed");
+    }
+
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("QuizActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("QuizActivity", "Received connection message: " + message);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                SpotifyApi api = new SpotifyApi();
+                api.setAccessToken(response.getAccessToken());
+                spotify = api.getService();
+                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                    @Override
+                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+                        mPlayer = spotifyPlayer;
+                        mPlayer.addConnectionStateCallback(QuizActivity.this);
+                        mPlayer.addNotificationCallback(QuizActivity.this);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("QuizActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        }
     }
 
     private void loadQuizData(){
